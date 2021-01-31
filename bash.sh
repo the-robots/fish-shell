09c54000c711c7654d67b76ae735cbb4b1a625ed
Melanie Cooper <melscoop@github.com>
2021-01-31 16:04:21 -0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,12 @@
+# making sure no pass is required for sudo
 echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
+
+# installing fish package
+
 sudo apt install fish -y
-fish 
+
+# changing shell to fish
+sudo chsh -s /usr/bin/fish
+
+# testing fish version
+fish -v
